c765e524f7e4a073c0b5e8955dcbfdad7e1423c1
aqrl-alessandro <alessandrobarreto1201@gmail.com>
2020-11-22 18:53:41 -0300

    fix wand map method

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-
 fetch("http://hp-api.herokuapp.com/api/characters")
-.then(function(response) {
-    return response.json();
-  })
-.then(function(data) {
-    console.log(data)
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
     //agora que começa a brincadeira
-    const generateHTML = data.map((api)=>{
-        function cor(){
-            if (api.house==="Gryffindor"){
-                return "grifinoria"
-            }else if(api.house==="Slytherin"){
-                return "sonserina"
-            }else if(api.house==="Ravenclaw"){
-                return "corvinal"
-            }else if (api.house==='Hufflepuff'){
-                return "lufalufa"
-            }else{
-                api.house="⠀⠀⠀⠀⠀⠀⠀⠀"
-                return "semcasa"
-            }    
-        }   
-        
-        return `
-            <div class="${cor()} batata">
-            <p class="nome" >${api.name}</p>
-            <p class="house">${api.house}</p>
-            <a href="https://www.w3schools.com" target=><img class="image" src=${api.image} alt="lalala" > </a>
-            </div>
-        `
-    })
-        document.getElementById("data").innerHTML=generateHTML
-        
-        
-});
+    const generateHTML = data
+      .map((api) => {
+        function houseColor() {
+          if (api.house === "Gryffindor") {
+            return "grifinoria";
+          } else if (api.house === "Slytherin") {
+            return "sonserina";
+          } else if (api.house === "Ravenclaw") {
+            return "corvinal";
+          } else if (api.house === "Hufflepuff") {
+            return "lufalufa";
+          } else {
+            api.house = "sem casa";
+            return "semcasa";
+          }
+        }
 
+        return `
+            <div class=" flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front ${houseColor()}">
+                        <p class="name-character" >${api.name}</p>
+                        <p class="house-character">${api.house}</p>
+                        <img class="image-character" src=${api.image} /> 
+                    </div>
+                    <div class="flip-card-back ${houseColor()}">
+                        <p>${api.dateOfBirth}</p>
+                        <p>${api.eyeColour}</p>
+                        <p>${api.hairColour}</p>
+                        <p>${api.ancestry}</p>
+                        <p>${api.patronus}</p>
+                        ${Object.values(api.wand).map((e) => {
+                          return `<p>${e}</p>`;
+                        }).join("")}
+                    </div>
+                </div>
+            </div>
+        `;
+      })
+      .join("");
+    document.getElementById("data").innerHTML = generateHTML;
+  });
